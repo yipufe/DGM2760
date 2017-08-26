a54701d16013035aa38d365d6c5b9c28e753c3e7
unknown <rdmanwill@gmail.com>
2017-08-26 13:28:49 -0600

    Updates

--- Unit1/js/main.js
+++ Unit1/js/main.js
@@ -1,9 +1,7 @@
+"use strict";
+/*global document,$,prompt*/
 
-var yourName = prompt("What is your name?", "John Doe");
-if(yourName=="null")
-	yourName = "";
-else
-	yourName = " "+yourName;
+var yourName = prompt("What is your name?", "John");
 
 var companyName = "Joe's Bed and Breakfast";
 var companySlogan = "Your comfort is our quest";
@@ -12,10 +10,9 @@ var currentDate =  (timeAtLoad.getMonth()+1)+"/"+
 	timeAtLoad.getDate()+"/"+
 	timeAtLoad.getFullYear();
 
-$(document).ready(function() {
+$(function() {
 	document.getElementById("persons_name").innerHTML = yourName;
 	document.getElementById("c_name").innerHTML = companyName;
 	document.getElementById("c_slogan").innerHTML = companySlogan;	
-	document.getElementById("date_now").innerHTML = currentDate;	
-
+	document.getElementById("date_now").innerHTML = currentDate;
 });
